3119ab13d05d2378169423a2a0e2a97eec317ca2
luxe0x64 <123239129+luxe0x64@users.noreply.github.com>
2025-03-10 22:00:57 +0100

    Update NBdownloader.py
    Version: 1.5

--- NBdownloader.py
+++ NBdownloader.py
@@ -1,21 +1,25 @@
 from pytubefix import YouTube
+from os import system
+
 
 #######################################################################################
 #                          Created by: nobody3132 aka luxe0x64                        #
 #                                      Date: 14.02.2025                               #
 #                              Happy Saint Valentine's day!!!                         #
-#                                                                                     #
+#                                       Version: 1.5                                  #
 #######################################################################################
 
 
 class TubeDownloader:
 
     def __init__(self):
+        system('clear')
         self.link = None
         self.playList = None
     pass
 
     def Downloading(self):
+        system('cat NBDownloader_banner.txt')
         self.playList = input("Playlist? \nY/n: ")
         if self.playList == "Yes" or self.playList == "yes" or self.playList == "y" or self.playList == "Y":
             self.link = input("link: ")
